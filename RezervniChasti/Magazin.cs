e0a04118a28e9f53734ab5569328f0fde4ddcce9
Your Name <you@example.com>
2023-06-09 12:39:35 +0300

    First Commit

---
+++ RezervniChasti/Magazin.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RezervniChasti
+{
+    public class Magazin
+    {
+        public List<RezervnaChast> RezervniChasti { get; set; }
+        public object Kolichestvo { get; private set; }
+
+        public Magazin()
+        {
+            RezervniChasti = new List<RezervnaChast>();
+        }
+
+
+        public void NapishiRezervnaChast(string VidStoka, string MarkaKoli, int Kolichestvi, decimal Cena, string MernaEdinica)
+        {
+            RezervnaChast novaChast = null;
+
+            if (VidStoka == "Dvigatel")
+            {
+                novaChast = new Dvigatel()
+                {
+                    VidStoka = VidStoka,
+                    MarkaKoli = MarkaKoli,
+                    Kolichestvo = Kolichestvo,
+                    Cena = Cena,
+                    MernaEdinica = MernaEdinica
+                };
+            }
+            else if (VidStoka == "Gumi")
+            {
+                novaChast = new Gumi()
+                {
+                    VidStoka = VidStoka,
+                    MarkaKoli = MarkaKoli,
+                    Kolichestvo = Kolichestvo,
+                    Cena = Cena,
+                    MernaEdinica = MernaEdinica
+                };
+
+            }
+
+            else if (VidStoka == "Spirachki")
+            {
+                novaChast = new Spirachki()
+                {
+                    VidStoka = VidStoka,
+                    MarkaKoli = MarkaKoli,
+                    Kolichestvo = Kolichestvo,
+                    Cena = Cena,
+                    MernaEdinica = MernaEdinica
+                };
+            }
+
+            if (novaChast != null)
+            {
+                RezervniChasti.Add(novaChast);
+            }
+        }
+    }
+}
---
+++ RezervniChasti/RezervnaChast.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RezervniChasti
+{
+    public abstract class RezervnaChast: IPrint
+    {
+        private string vidStoka;
+        private string markaKoli;
+        private int kolichestvo;
+        private decimal cena;
+        private string mernaEdinica;
+        
+
+        public string VidStoka
+        {
+            get { return vidStoka; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("value");
+                }
+                this.vidStoka = value;
+            }
+        }
+
+        public string MarkaKoli
+        { 
+            get { return markaKoli; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("value");
+                }
+                this.markaKoli = value;
+            }
+        }
+        public int Kolichestvo
+        {
+            get { return kolichestvo; }
+            set
+            {
+                if (value<0)
+                {
+                    throw new ArgumentException("value");
+                }
+                this.kolichestvo = value;
+            }
+        }
+        public decimal Cena
+        {
+            get { return cena; }
+            set
+            {
+                if (value<0)
+                {
+                    throw new ArgumentException("value");
+                }
+                this.cena = value;
+            }
+            
+        }
+        public string MernaEdinica
+        {
+            get { return mernaEdinica; }
+            set
+            {
+                if (string.IsNullOrEmpty ("value"))
+                {
+                    throw new ArgumentException("value");
+                }  
+                this.mernaEdinica = value;
+            }
+        }
+        public RezervnaChast(string vidStoka, string markaKoli, 
+        int kolichestvo, decimal cena, string mernaEdinica )
+        {
+            this.VidStoka=vidStoka;
+            this.MarkaKoli=markaKoli;
+            this.Kolichestvo=kolichestvo;
+            this.Cena=cena;
+            this.MernaEdinica=mernaEdinica;
+            
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine($"{this.vidStoka} {this.markaKoli}" +
+                $" {this.kolichestvo} {this.cena} {this.mernaEdinica}");
+        }
+
+        public abstract decimal IzchisliSrednoKolichestvo();
+
+    }
+
+}
